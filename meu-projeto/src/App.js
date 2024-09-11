f42754cccfddc26f6ef5dad2ff91f2622b6fd6c8
Everaldo-ifrn <99830593+Everaldo-ifrn@users.noreply.github.com>
2024-09-11 15:52:09 -0300

    mais estudo

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,10 @@ import HelloWorld from "./components/HelloWorld"
 import Saudacao from "./components/Saudacao"
 // para usar-lo basta: <HelloWorld />
 
+// os componentes são cada parte do site, tipo: hero, footer...
+// os css de cada componente é: [nomeComponente].module.css
+// fazer o site completo no app, e as partes do site nos componentes
+
 function App() {
   const url = 'https://via.placeholder.com/150'
   const nome = "Everaldo"
